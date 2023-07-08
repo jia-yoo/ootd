e0a38c699eb2e57e1957818ad1f916e64f0b14e1
jia-yoo <jiayoo.dev@gmail.com>
2023-07-08 15:35:50 +1000

    Implemented redux-persist as well as redux-dev tool

--- src/store/cart/cart.reducer.js
+++ src/store/cart/cart.reducer.js
@@ -14,6 +14,7 @@ export const cartReducer = (state = CART_INITIAL_STATE, action = {}) => {
       return { ...state, cartItems: payload };
     case CART_ACTION_TYPES.SET_IS_CART_OPEN:
       return { ...state, isCartOpen: payload };
+    
     default:
       return state;
   }
--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,28 @@ import logger from 'redux-logger'
 
 import { rootReducer } from './root-reducer'
 
-//root-reducer
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 
-const middleWares = [logger]
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'],  
+}
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
+const middleWares = [process.env.NODE_ENV === 'development'&& logger].filter(Boolean)
  
-const composedEnhancers = compose(applyMiddleware(...middleWares))
+const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
  
-export const store = createStore(rootReducer, undefined, composedEnhancers)
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares))
+
+export const store = createStore(persistedReducer, undefined, composedEnhancers)
+
+ 
+export const persistor = persistStore(store)
